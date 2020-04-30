128877e7a39250a240caf78192882b48e9bc689e
zhouhaixin-123 <1514768257@qq.com>
2020-04-30 16:08:57 +0800

    develop:添加学生姓名和年龄属性的get和set方法，创建StudentDao类

--- src/main/java/com/git/pojo/Student.java
+++ src/main/java/com/git/pojo/Student.java
@@ -13,4 +13,22 @@ public class Student {
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+	
 }
